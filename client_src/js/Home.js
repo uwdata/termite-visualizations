5ed91a7c5691ec1edcaf16780fff392b33d28ad3
Jason Chuang <jcchuang@cs.stanford.edu>
2014-02-07 18:16:46 -0800

    Update homepage.

--- client_src/js/Home.js
+++ client_src/js/Home.js
@@ -1,13 +1,13 @@
 var Home = Backbone.Model.extend({
 	defaults : {
-		server : "http://127.0.0.1:8075",
+		server : "127.0.0.1:8075",
 		dataset : null,
-		datasets : []
+		datasets : [ null ]
 	},
 	url : function() {
 		var server = this.get( "server" );
 		var dataset = this.get( "dataset" );
-		var url = server;
+		var url = "http://" + server;
 		if ( dataset ) { url += "/" + dataset }
 		url += "?format=json"
 		return url;
@@ -20,7 +20,7 @@ Home.prototype.initialize = function() {
 
 Home.prototype.load = function() {
 	var success = function( model, response, options ) {
-		var datasets = [];
+		var datasets = [ null ];
 		if ( response.configs.datasets ) {
 			for ( var i = 0; i < response.configs.datasets.length; i++ ) {
 				datasets.push( response.configs.datasets[i] );
@@ -31,6 +31,7 @@ Home.prototype.load = function() {
 			"dataset" : response.configs.dataset,
 			"datasets" : datasets
 		})
+		console.log( "success" );
 	}.bind(this);
 	var error = function( model, response, options ) {
 		console.log( "error" );
@@ -41,7 +42,8 @@ Home.prototype.load = function() {
 var HomeView = Backbone.View.extend();
 
 HomeView.prototype.initialize = function() {
-	this.setServer = _.debounce( this.__setServer, 2000, false );
+	this.setServer = _.debounce( this.__setServer, 50 );
+	this.delaySetServer = _.debounce( this.setServer, 2500, false );
 	this.layer = {};
 	this.layer.server = d3.select( ".ServerView" );
 	this.layer.datasets = d3.select( ".DatasetsView" )
@@ -49,14 +51,21 @@ HomeView.prototype.initialize = function() {
 	this.initServer();
 	this.initDatasets();
 	this.model.on( "change:server", this.updateServer, this );
-	this.model.on( "change:dataset change:datasets", this.updateDatasets, this );
+	this.model.on( "change:dataset", this.updateDataset, this );
+	this.model.on( "change:datasets", this.updateDatasets, this );
 };
 
 HomeView.prototype.initServer = function() {
 	var layer = this.layer.server;
 	this.io.server = layer.append( "input" )
 		.attr( "type", "text" )
-		.on( "keyup", this.setServer.bind(this) );
+		.on( "keyup", this.delaySetServer.bind(this) );
+	this.io.refresh = layer.append( "i" )
+		.attr( "class", "fa fa-refresh" )
+		.style( "margin", "0 5px" )
+		.style( "font-size", "0.8em" )
+		.style( "cursor", "pointer" )
+		.on( "click", this.setServer.bind(this) );
 	this.updateServer();
 };
 
@@ -81,16 +90,27 @@ HomeView.prototype.updateDatasets = function() {
 	var elems = select.selectAll( "option" ).data( datasets );
 	elems.enter().append( "option" )
 		.attr( "selected", function(d) { return d === dataset ? "selected" : null } )
-		.attr( "value", function(d) { return d } )
-		.html( function(d) { return d } );
+		.attr( "value", function(d) { return (d===null) ? "NONE" : d } )
+		.html( function(d) { return (d===null) ? "&mdash; Dataset &mdash;" : d } );
+};
+
+HomeView.prototype.updateDataset = function() {
+	var dataset = this.model.get( "dataset" );
+	this.io.dataset.node().value = dataset;
 };
 
 HomeView.prototype.__setServer = function() {
 	var value = this.io.server.node().value;
-	console.log( value )
+	this.model.set( "server", value );
+	this.model.load();
 };
 
 HomeView.prototype.setDataset = function() {
 	var value = this.io.dataset.node().value;
-	console.log( value )
+	if ( value === "NONE" ) {
+		this.model.set( "dataset", null );
+	}
+	else {
+		this.model.set( "dataset", value );
+	}
 };
